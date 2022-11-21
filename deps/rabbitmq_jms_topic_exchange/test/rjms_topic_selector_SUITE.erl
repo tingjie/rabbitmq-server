6fc9f2d70f43794ac6551a45eb75dbb359b1e315
Diana Parra Corbacho <dparracorbac@vmware.com>
2022-11-21 18:57:39 +0100

    Khepri: prepare jms topic tests to run with khepri flag enabled

--- deps/rabbitmq_jms_topic_exchange/test/rjms_topic_selector_SUITE.erl
+++ deps/rabbitmq_jms_topic_exchange/test/rjms_topic_selector_SUITE.erl
@@ -45,14 +45,16 @@ init_per_suite(Config) ->
 end_per_suite(Config) ->
     rabbit_ct_helpers:run_teardown_steps(Config).
 
-init_per_group(mnesia_store = Group, Config) ->
+init_per_group(mnesia_store = Group, Config0) ->
+    Config = rabbit_ct_helpers:set_config(Config0, [{metadata_store, mnesia}]),
     init_per_group_common(Group, Config);
 init_per_group(khepri_store = Group, Config0) ->
     Config = rabbit_ct_helpers:set_config(
                Config0,
                [{metadata_store, {khepri, [rabbit_jms_topic_exchange_raft_based_metadata_store]}}]),
     init_per_group_common(Group, Config);
-init_per_group(khepri_migration = Group, Config) ->
+init_per_group(khepri_migration = Group, Config0) ->
+    Config = rabbit_ct_helpers:set_config(Config0, [{metadata_store, mnesia}]),
     init_per_group_common(Group, Config).
 
 init_per_group_common(Group, Config) ->
